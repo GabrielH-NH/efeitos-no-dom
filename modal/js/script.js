89a25133572a02066393c7e04711b0e14e3b2323
GabrielH-NH <gabrielhenriquenh@gmail.com>
2024-01-18 22:08:54 -0300

    Aprendendo a criar um modal

---
+++ modal/js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modulos/scroll-suave.js'
+import initAnimaScroll from './modulos/scroll-animacao.js'
+import initAccordion from './modulos/accordion.js'
+import initTabNav from './modulos/tabnav.js'
+
+initScrollSuave();
+initAnimaScroll();
+initAccordion();
+initTabNav();

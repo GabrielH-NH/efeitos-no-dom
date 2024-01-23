395468a769ff75c774ae5425a777f1f447d726f3
GabrielH-NH <gabrielhenriquenh@gmail.com>
2024-01-22 21:29:14 -0300

    Terminando o modal

--- modal/js/script.js
+++ modal/js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modulos/scroll-suave.js'
 import initAnimaScroll from './modulos/scroll-animacao.js'
 import initAccordion from './modulos/accordion.js'
 import initTabNav from './modulos/tabnav.js'
+import initModal from './modulos/modal.js'
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
